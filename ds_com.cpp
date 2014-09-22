9b64818c1559d3a9a3fafc3d51647d6133840a84
alexr9000 <vagrant@embedded-dev.(none)>
2014-09-22 21:44:42 +0000

    Hacked version that sends IMU Magnetometer data.

--- ds_com.cpp
+++ ds_com.cpp
@@ -11,13 +11,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
-
 #include <stdlib.h>
 
 #include "imu_gyro.h"
 #include "imu_mag.h"
+#include "imu_sensor.h"
 
 #define ARRAY_LEN(x) (sizeof(x) / sizeof((x)[0]))
+#define DEFAULT_TTYDEV "/dev/ttyS2"  // Telem 2 on pixhawk.
 
 extern "C" __EXPORT int ds_com_main(int argc, char *argv[]);
 
@@ -28,19 +29,25 @@ void print_usage() {
 }
 
 int ds_com_main(int argc, char * argv[]) {
-/*
+
     uint8_t status;
     int iterations = 10;
     int delay_duration = 0;
-
     int c;
 
-    auto mag_print = [](mag_report & report) {
-        printf("\t MAG values: x:%8.4f \t y:%8.4f \t z:%8.4f\n", (double) report.x, (double) report.y, (double) report.z);
-        printf("\t MAG values: x_raw:%d \t y_raw:%d \t z_raw:%d\n\n", report.x_raw, report.y_raw, report.z_raw);
+    union data_to_send {
+        unsigned char byte[2];
+        uint16_t word;
+    };
+    data_to_send data;
+
+    // Start with magnetometer (int16_t) z_raw.  It should vary between 6000 and 400
+    auto mag_print = [](mag_report &report) {
+        //printf("\t MAG values: x:%8.4f \t y:%8.4f \t z:%8.4f\n", (double) report.x, (double) report.y, (double) report.z);
+        printf("\t MAG values: x_raw:%d \t y_raw:%d \t z_raw:%d\n", report.x_raw, report.y_raw, report.z_raw);
     };
 
-    auto gyro_print = [](gyro_report & report) {
+    auto gyro_print = [](gyro_report &report) {
         printf("\t Gyro values: x:%8.4f \t y:%8.4f \t z:%8.4f\n", (double) report.x, (double) report.y, (double) report.z);
         printf("\t Gyro values: x_raw:%8d \t y_raw:%8d \t z_raw:%8d\n\n", report.x_raw, report.y_raw, report.z_raw);
     };
@@ -59,28 +66,27 @@ int ds_com_main(int argc, char * argv[]) {
         return 1;
     }
 
-    //for (int i = 0; i < 20; i++) {
+    /*
+    for (int i = 0; i < 5000; i++) {
         mag.read_report();
         gyro.read_report();
-    //}
-    printf ("Hello Skycatch!\n");
+        sleep(1);
+    }
     */
+    printf ("Hello Skycatch!\n");
+    
 
     int fd = open("/dev/ttyS2", O_RDWR | O_NOCTTY | O_NDELAY);
-    //int fd = open("/dev/ttyS2", O_RDWR | O_NOCTTY);
-
     if (fd < 0) {
         printf ("Failed to open fd\n");
         return (-1);
     }
 
-	/* Try to set baud rate */
+    // Configure the serial port
 	struct termios uart_config;
-
-	/* Fill the struct for the new configuration */
 	tcgetattr(fd, &uart_config);
 
-	/* Clear ONLCR flag (which appends a CR for every LF) */
+	// Clear ONLCR flag (which appends a CR for every LF)
 	uart_config.c_oflag &= ~ONLCR;
 
 	if (cfsetispeed(&uart_config, B57600) < 0 || cfsetospeed(&uart_config, B57600) < 0) {
@@ -93,35 +99,32 @@ int ds_com_main(int argc, char * argv[]) {
 		return 1;
     }
 
-    unsigned char data[1];
-    data[0] = 65;
-    int res, count = 0;
+
+//    data.byte[0] = 65;
+//    data.byte[1] = 65;
+    int res;
     while (1) {
-        res = write(fd, &data[0], 1);
+        mag.read_report();
+        data.word = mag.m_z_raw;
+
+        res = write(fd, &data.byte[0], 2);
         if(res < 0) {
-            printf("ERROR writing %x\n", data[0]);
+            printf("ERROR writing %x\n", data.byte[0]);
             return 1;
         }
         
-        //if ((data[0] >= 0x6c) && (data[0] <= 0x6e)) {
-        //    printf ("Sending 0x%x\n", data[0]);
-        //}
-        data[0]++;
-        if (data[0] == 91) {
-            data[0] = 65;
-        } 
-
         /*
-        if (count++ == 1000) {
-            data[0] = 0;
-            count = 0;
-        }
+        data.byte[0]++;
+        data.byte[1]++;
+        if (data.byte[0] == 91) {
+            data.byte[0] = 65;
+            data.byte[1] = 65;
+        } 
         */
         usleep(5000);
     }
     
     close(fd);
-
     return 0;
 }
 
