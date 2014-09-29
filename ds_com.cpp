e03aa48797459830d3c95240b6dfc246b98bc92b
alxxram <vagrant@embedded-dev.(none)>
2014-09-29 21:23:15 +0000

    Added gyro and accel

--- ds_com.cpp
+++ ds_com.cpp
@@ -44,6 +44,36 @@ float min_mag_z = 0;
 int16_t max_raw_mag_z = 0;
 int16_t min_raw_mag_z = 0;
 
+float max_gyro_x = 0;
+float min_gyro_x = 0;
+int16_t max_raw_gyro_x = 0;
+int16_t min_raw_gyro_x = 0;
+
+float max_gyro_y = 0;
+float min_gyro_y = 0;
+int16_t max_raw_gyro_y = 0;
+int16_t min_raw_gyro_y = 0;
+
+float max_gyro_z = 0;
+float min_gyro_z = 0;
+int16_t max_raw_gyro_z = 0;
+int16_t min_raw_gyro_z = 0;
+
+float max_accel_x = 0;
+float min_accel_x = 0;
+int16_t max_raw_accel_x = 0;
+int16_t min_raw_accel_x = 0;
+
+float max_accel_y = 0;
+float min_accel_y = 0;
+int16_t max_raw_accel_y = 0;
+int16_t min_raw_accel_y = 0;
+
+float max_accel_z = 0;
+float min_accel_z = 0;
+int16_t max_raw_accel_z = 0;
+int16_t min_raw_accel_z = 0;
+
 template<typename T>
 bool check_max(T &max_x, T &max_y, T &max_z,
                T   in_x, T   in_y, T   in_z)
@@ -108,11 +138,33 @@ int ds_com_main(int argc, char * argv[]) {
     auto gyro_print = [](gyro_report &report) {
         //printf("\t GyroX:%8.4f \t GyroY:%8.4f \t GyroZ:%8.4f \t", (double) report.x, (double) report.y, (double) report.z);
         //printf("\t GyroX %d \t GyroY %d \t GyroZ %d  \t", report.x_raw, report.y_raw, report.z_raw);
+        if (check_max(max_gyro_x, max_gyro_y, max_gyro_z, report.x, report.y, report.z) ||
+            check_min(min_gyro_x, min_gyro_y, min_gyro_z, report.x, report.y, report.z) ||
+            check_max(max_raw_gyro_x, max_raw_gyro_y, max_raw_gyro_z, report.x_raw, report.y_raw, report.z_raw) ||
+            check_min(min_raw_gyro_x, min_raw_gyro_y, min_raw_gyro_z, report.x_raw, report.y_raw, report.z_raw)) {
+            printf("GyroX_max:%8.4f GyroX_min:%8.4f  ", (double)max_gyro_x, (double)min_gyro_x);
+            printf("GyroY_max:%8.4f GyroY_min:%8.4f  ", (double)max_gyro_y, (double)min_gyro_y);
+            printf("GyroZ_max:%8.4f GyroZ_min:%8.4f\n", (double)max_gyro_z, (double)min_gyro_z);
+            printf("GyroX_raw_max:%d GyroX_raw_min:%d  ", max_raw_gyro_x, min_raw_gyro_x);
+            printf("GyroY_raw_max:%d GyroY_raw_min:%d  ", max_raw_gyro_y, min_raw_gyro_y);
+            printf("GyroZ_raw_max:%d GyroZ_raw_min:%d\n", max_raw_gyro_z, min_raw_gyro_z);
+        }
     };
 
     auto accel_print = [](accel_report & report) {
-        printf("\t AccelX:%8.4f \t AccelY:%8.4f \t AccelZ:%8.4f\n", (double) report.x, (double) report.y, (double) report.z);
+        //printf("\t AccelX:%8.4f \t AccelY:%8.4f \t AccelZ:%8.4f\n", (double) report.x, (double) report.y, (double) report.z);
         //printf("\t AccelX %d \t AccelY %d \t AccelZ %d \n", report.x_raw, report.y_raw, report.z_raw);
+        if (check_max(max_accel_x, max_accel_y, max_accel_z, report.x, report.y, report.z) ||
+            check_min(min_accel_x, min_accel_y, min_accel_z, report.x, report.y, report.z) ||
+            check_max(max_raw_accel_x, max_raw_accel_y, max_raw_accel_z, report.x_raw, report.y_raw, report.z_raw) ||
+            check_min(min_raw_accel_x, min_raw_accel_y, min_raw_accel_z, report.x_raw, report.y_raw, report.z_raw)) {
+            printf("AccelX_max:%8.4f AccelX_min:%8.4f  ", (double)max_accel_x, (double)min_accel_x);
+            printf("AccelY_max:%8.4f AccelY_min:%8.4f  ", (double)max_accel_y, (double)min_accel_y);
+            printf("AccelZ_max:%8.4f AccelZ_min:%8.4f\n", (double)max_accel_z, (double)min_accel_z);
+            printf("AccelX_raw_max:%d AccelX_raw_min:%d  ", max_raw_accel_x, min_raw_accel_x);
+            printf("AccelY_raw_max:%d AccelY_raw_min:%d  ", max_raw_accel_y, min_raw_accel_y);
+            printf("AccelZ_raw_max:%d AccelZ_raw_min:%d\n", max_raw_accel_z, min_raw_accel_z);
+        }
     };
 
     IMU_Mag mag(mag_print);
@@ -164,9 +216,9 @@ int ds_com_main(int argc, char * argv[]) {
     int res;
     data.byte[0] = 65;
     while (1) {
-        mag.read_report();
+        //mag.read_report();
         //gyro.read_report();
-        //accel.read_report();
+        accel.read_report();
 
         /*
         res = write(fd, &data.byte[0], 1);
@@ -181,9 +233,9 @@ int ds_com_main(int argc, char * argv[]) {
         if (data.byte[0] == 91) {
             data.byte[0] = 65;
         } 
-
-        usleep(5000);
         */
+
+        usleep(500);
     }
     
     close(fd);
