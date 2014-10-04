dc290e7a48196036df128580ec6e3ab4da2a31f4
alxxram <alexr@skycatch.com>
2014-10-04 00:22:17 +0000

    Added iterations and error injection

--- ds_com.cpp
+++ ds_com.cpp
@@ -24,38 +24,52 @@
 extern "C" __EXPORT int ds_com_main(int argc, char *argv[]);
 
 void print_usage() {
-    printf("imu_cap -i <iterations> -d <delay in ms>\n\n");
-    printf("imu_cap is a program to read board IMU sensors at a high rate\n");
+    printf("ds_com -e\t\tTo inject errors\n");
+    printf("ds_com -s\t\tTo print sensor data to the screen\n");
+    printf("ds_com -i\t\tThe number of iterations to loop through\n\n");
+    printf("ds_com sends the alphabet forever on port: %s\n", DEFAULT_TTYDEV);
+    printf("ds_com can also print magnetometer, accelerometer, and");
+    printf(" gyro sensor data to the screen\n");
     printf("Build: %s\n", __TIME__);
 }
 
 int ds_com_main(int argc, char * argv[]) {
 
     uint8_t status;
-    int iterations = 10;
-    int delay_duration = 0;
-    int c;
+    int err_inj = 0;
+    int log_sensors = 0;
+    int c, iterations = 1000;
 
-    union data_to_send {
-        unsigned char byte[6];
-        uint16_t word[3];
-    };
-    data_to_send data;
+    while((c = getopt(argc, argv, "esi:")) != -1) {
+        switch (c) {
+            case 'e':
+                err_inj = 1;
+            break;
+            case 's':
+                log_sensors = 1;
+            break;
+            case 'i':
+                iterations = strtoul(optarg, NULL, 10);
+            break;
+            default:
+                print_usage();
+                return 1;
+        }
+    }
 
-    // Start with magnetometer (int16_t) z_raw.  It should vary between 6000 and 400
     auto mag_print = [](mag_report &report) {
-        //printf("MagX:%8.4f \t MagY:%8.4f \t MagZ:%8.4f \t", (double) report.x, (double) report.y, (double) report.z);
-        //printf("\t MagX %d \t MagY %d \t MagZ %d  \t", report.x_raw, report.y_raw, report.z_raw);
+        printf("MagX:%8.4f \t MagY:%8.4f \t MagZ:%8.4f \t", (double) report.x, (double) report.y, (double) report.z);
+        printf("\t MagX %d \t MagY %d \t MagZ %d  \t", report.x_raw, report.y_raw, report.z_raw);
     };
 
     auto gyro_print = [](gyro_report &report) {
-        //printf("\t GyroX:%8.4f \t GyroY:%8.4f \t GyroZ:%8.4f \t", (double) report.x, (double) report.y, (double) report.z);
-        //printf("\t GyroX %d \t GyroY %d \t GyroZ %d  \t", report.x_raw, report.y_raw, report.z_raw);
+        printf("\t GyroX:%8.4f \t GyroY:%8.4f \t GyroZ:%8.4f \t", (double) report.x, (double) report.y, (double) report.z);
+        printf("\t GyroX %d \t GyroY %d \t GyroZ %d  \t", report.x_raw, report.y_raw, report.z_raw);
     };
 
     auto accel_print = [](accel_report & report) {
         printf("\t AccelX:%8.4f \t AccelY:%8.4f \t AccelZ:%8.4f\n", (double) report.x, (double) report.y, (double) report.z);
-        //printf("\t AccelX %d \t AccelY %d \t AccelZ %d \n", report.x_raw, report.y_raw, report.z_raw);
+        printf("\t AccelX %d \t AccelY %d \t AccelZ %d \n", report.x_raw, report.y_raw, report.z_raw);
     };
 
     IMU_Mag mag(mag_print);
@@ -81,37 +95,42 @@ int ds_com_main(int argc, char * argv[]) {
 
     printf ("Hello Skycatch!\n");
     
-    int fd = open("/dev/ttyS2", O_RDWR | O_NOCTTY | O_NDELAY);
+    int fd = open(DEFAULT_TTYDEV, O_RDWR | O_NOCTTY | O_NDELAY);
     if (fd < 0) {
         printf ("Failed to open fd\n");
-        return (-1);
+        return (1);
     }
 
     // Configure the serial port
 	struct termios uart_config;
 	tcgetattr(fd, &uart_config);
 
-	// Clear ONLCR flag (which appends a CR for every LF)
-	uart_config.c_oflag &= ~ONLCR;
-
 	if (cfsetispeed(&uart_config, B57600) < 0 || cfsetospeed(&uart_config, B57600) < 0) {
 		printf("ERROR setting baud\n");
 		return 1;
 	}
+
+	// Clear ONLCR flag (which appends a CR for every LF)
+	uart_config.c_oflag &= ~ONLCR;
+
+    uart_config.c_cflag |= CS8 | CLOCAL;
+    uart_config.c_cflag &= ~(CRTSCTS | PARENB | CSTOPB);
 
 	if (tcsetattr(fd, TCSANOW, &uart_config) < 0) {
 		printf("ERROR setting attr\n");
 		return 1;
     }
 
-    int res;
-    data.byte[0] = 65;
-    while (1) {
-        //mag.read_report();
-        //gyro.read_report();
-        //accel.read_report();
+    unsigned char data = 'A';
+    for (int i = 0; i < iterations; i++) {
 
-        res = write(fd, &data.byte[0], 1);
+        if (log_sensors == 1) {
+            mag.read_report();
+            gyro.read_report();
+            accel.read_report();
+        }
+
+        int res = write(fd, &data, 1);
         if(res < 0) {
             printf("ERROR writing\n");
             return 1;
@@ -119,10 +138,19 @@ int ds_com_main(int argc, char * argv[]) {
             continue;
         }
         
-        data.byte[0]++;
-        if (data.byte[0] == 91) {
-            data.byte[0] = 65;
-        } 
+        data++;
+        if (data == 'Z' + 1) {
+            data = 'A';
+        }
+
+        // If error injection is enabled
+        if (err_inj == 1) {
+            // Corrupt the "G" every time
+            if (data == 'G') {
+                data = 0;
+                printf("Sending zero\n");
+            }
+        }
 
         usleep(5000);
     }
